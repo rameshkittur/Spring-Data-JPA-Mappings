0898ebac27305d96e1c97940e6d73d3ba6a4d34a
Ramesh <rameshkittur.kn@gmail.com>
2024-12-30 15:22:32 +0530

    Initial commit

---
+++ src/main/java/com/rameshkittur/DataMappingJPA/DataMappingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.rameshkittur.DataMappingJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMappingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMappingJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rameshkittur/DataMappingJPA/DataMappingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rameshkittur.DataMappingJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMappingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
